99b9525ca32e4aacdd2e82adff02590f5709c68a
EstelleEvelyn <estelle.bayer@gmail.com>
2017-06-20 16:39:26 -0500

    Starting structure of a flask implementation for comparison of features with MND using bar graph

---
+++ bar.py
@@ -0,0 +1,72 @@
+import sys
+import flask
+import json
+import api_config
+import psycopg2
+import urllib
+
+app = flask.Flask(__name__)
+
+@app.route('/<char1>+<char2>+<char3>')
+def create_data(char1, char2, char3):
+
+    manner_data = [{},{},{}]
+    placement_data = [{},{},{}]
+    voice_data = [{},{},{}]
+
+    manner_key = []
+    placement_key = []
+    voice_key = []]
+
+
+    files = [[][][]]
+
+    files[0][0] = "features/{}_manner.csv".format(char1)
+    files[0][1] = "features/{}_manner.csv".format(char2)
+    files[0][2] = "features/{}_manner.csv".format(char3)
+
+    files[1][0] = "features/{}_placement.csv".format(char1)
+    files[1][1] = "features/{}_placement.csv".format(char2)
+    files[1][2] = "features/{}_placement.csv".format(char3)
+
+    files[2][0] = "features/{}_voice.csv".format(char1)
+    files[2][1] = "features/{}_voice.csv".format(char2)
+    files[2][2] = "features/{}_voice.csv".format(char3)
+
+    manner_data[0]['Character'] = char1
+    manner_data[1]['Character'] = char2
+    manner_data[2]['Character'] = char3
+
+    placement_data[0]['Character'] = char1
+    placement_data[1]['Character'] = char2
+    placement_data[2]['Character'] = char3
+
+    voice_data[0]['Character'] = char1
+    voice_data[1]['Character'] = char2
+    voice_data[2]['Character'] = char3
+
+    for i in range(2):
+        manner_data[i]['Total'] = 100
+        placement_data[i]['Total'] = 100
+        voice_data[i]['Total'] = 100
+
+    for j in range(2):
+        with open(files[0][j]) as manner_file:
+            manner_reader = csv.DictReader(manner_file)
+            for m_row in manner_reader:
+                manner_data[m_row.get('feature')] = m_row.get('percent')
+                manner_key.add(m_row.get('feature'))
+
+        with open(files[1][j]) as placement_file:
+            placement_reader = csv.DictReader(placement_file)
+            for p_row in placement_reader:
+                placement_data[p_row.get('feature')] = p_row.get('percent')
+                placement_key.add(p_row.get('feature'))
+
+        with open(files[2][j]) as voice_file:
+            voice_reader = csv.DictReader(voice_file)
+            for v_row in voice_reader:
+                voice_data[v_row.get('feature')] = v_row.get('percent')
+                voice_key.add(v_row.get('feature'))
+
+    
---
+++ static/bar_chart.js
@@ -0,0 +1,5 @@
+function onClick() {
+  var char1 = document.getElementById('Character1');
+  var char2 = document.getElementById('Character2');
+  var char3 = document.getElementById('Character3');
+}
