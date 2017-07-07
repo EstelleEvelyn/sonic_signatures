b18d76247b23e21e48f110f7520b555c9dde1175
EstelleEvelyn <estelle.bayer@gmail.com>
2017-07-07 16:07:37 -0500

    Revamping z-score visualization, starting with creating a simple template to build upon

--- visualization/z_score_viz.py
+++ visualization/z_score_viz.py
@@ -2,6 +2,8 @@ import flask
 import json
 import urllib
 import csv
+import os
+import re
 
 app = flask.Flask(__name__)
 
@@ -9,15 +11,19 @@ app = flask.Flask(__name__)
 def default():
     return flask.render_template("z_score.html")
 
-@app.route("/load")
-def get_zzz():
-    ret_list = {}
-    with open("../classifier/phoneme_z_scores.csv") as source:
-        read = csv.DictReader(source)
-        for row in read:
-            ret_list[row.get('phoneme')] = float(row.get('protag'))
-    return flask.jsonify(ret_list)
-
+@app.route('/options/<play>')
+def list_options(play):
+    '''
+    Returns the list of characters in a given play
+    '''
+    character_list = []
+    for fn in os.listdir("../tagging/dest"):
+        if re.match(play+"_", fn) is not None:
+            lstrip_length = len(play)+1
+            character = fn[lstrip_length:-4]
+            character_list.append(character)
+    character_list.sort()
+    return flask.jsonify(character_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
